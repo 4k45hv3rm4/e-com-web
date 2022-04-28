ebbfd6262ee6181a8296905050b1bd78535f7584
Aakash Verma <4k45hr0ck5007@gmail.com>
2022-04-28 09:36:25 +0530

    update check overdue exists flag

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <br />
+    <input type="text" placeholder="Input here" @input="changeValue" />
+    {{ this.inputValue }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+  data() {
+    return {
+      inputValue: "",
+    };
+  },
+  methods: {
+    changeValue(e) {
+      console.log(e.target.value);
+      this.inputValue = e.target.value;
+    },
+  },
+};
+</script>
